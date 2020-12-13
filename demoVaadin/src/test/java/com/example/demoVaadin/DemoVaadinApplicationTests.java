72047087705d2cb623eeab4e1a0e4ae65ca0636c
coder <coder@abc.com>
2020-12-13 18:06:26 +0000

    Initial commit

---
+++ demoVaadin/src/test/java/com/example/demoVaadin/DemoVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoVaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
